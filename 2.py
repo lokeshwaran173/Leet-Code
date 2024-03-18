ece63c30fde8608852d02bbb537e1bb59873c08e
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2024-03-18 18:00:10 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        left = 0
+        dummy = cur = ListNode(-1)
+        while l1 or l2 or left:
+            left, sm = divmod(sum(l and l.val or 0 for l in (l1, l2)) + left, 10)
+            cur.next = cur = ListNode(sm)
+            l1 = l1 and l1.next
+            l2 = l2 and l2.next
+        return dummy.next
